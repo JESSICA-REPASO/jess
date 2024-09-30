0860c8716bd6eb61ed092690d5ab698b9570a71d
JESSICA-REPASO <jessicarepaso56@gmail.com>
2024-09-30 05:49:03 +0000

    done code

---
+++ Tasksheet111.java
@@ -0,0 +1,10 @@
+/** 
+ * The HelloWorldApp class implements an application that
+ * simply prints "KamustaMundo!" to standard output.
+ */
+class Tasksheet111 {
+    public static void main(String[] args) {
+        System.out.println("KamustaMundo!"); 
+        // Display the string.
+    }
+}
